7d0e663fe481b20cb5a7f2c870b982e22ebaf3cf
Chris Winters <chris@cwinters.com>
2005-07-04 02:40:24 +0000

    Import of CWINTERS/OpenInteract2-Action-RSS-0.10 from CPAN.
    gitpan-cpan-distribution: OpenInteract2-Action-RSS
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         CWINTERS/OpenInteract2-Action-RSS-0.10.tar.gz
    gitpan-cpan-author:       CWINTERS
    gitpan-cpan-maturity:     released

--- lib/OpenInteract2/Action/RSS.pm
+++ lib/OpenInteract2/Action/RSS.pm
@@ -4,6 +4,7 @@ package OpenInteract2::Action::RSS;
 
 use strict;
 use base qw( OpenInteract2::Action );
+use Digest::MD5              qw( md5_hex );
 use File::Spec::Functions    qw( catfile );
 use Log::Log4perl            qw( get_logger );
 use OpenInteract2::Constants qw( :log );
@@ -12,7 +13,7 @@ use OpenInteract2::Exception qw( oi_error );
 use URI;
 use XML::Feed;
 
-$OpenInteract2::Action::RSS::VERSION          = '0.03';
+$OpenInteract2::Action::RSS::VERSION          = '0.10';
 
 # Use this if the action does not define its own template
 $OpenInteract2::Action::RSS::DEFAULT_TEMPLATE = 'rss_feed_default';
@@ -28,24 +29,50 @@ sub _find_task {
     return 'process';
 }
 
-# NOTE: we don't need to bother with caching here -- execute() in the
-# parent class takes care of it for us
+# NOTE: we don't need to bother with caching the output content here
+# -- execute() in the parent class takes care of it for us
 
 sub process {
     my ( $self ) = @_;
     $log ||= get_logger( LOG_ACTION );
 
+    my ( $url, $template, $feed );
+    eval {
+        $url = $self->_get_feed_url();
+        $log->is_debug &&
+            $log->debug( "Will load from feed URL '$url'" );
+        $feed = $self->_load_feed( $url );
+        $template = $self->_get_template_name();
+        $log->is_debug &&
+            $log->debug( "Will send feed and data to template '$template'" );
+    };
+    return "$@"  if ( $@ );
+    $log->is_info && $log->info( "Got all necessary data, processing feed..." );
+    my $title = $self->_get_title( $feed ) || $feed->title;
+    my %params = (
+        feed  => $feed,
+        title => $title,
+    );
+    $self->_modify_template_params( \%params );
+    return $self->generate_content( \%params, { name => $template } );
+}
+
+
+sub _get_feed_url {
+    my ( $self ) = @_;
+    my $class = ref( $self );
     my $url = $self->param( 'feed_url' );
     unless ( $url ) {
         $log->error( "No 'feed_url' defined for action ", $self->name );
-        return "Cannot get feed: parameter 'feed_url' must be defined " .
-               "for action '", $self->name, "'. (See " .
-               "OpenInteract2::Action::RSS for details.)";
+        die "Cannot get feed: parameter 'feed_url' must be defined " .
+            "for action '", $self->name, "'. (See $class for details).\n";
     }
+    return $url;
+}
 
-    my ( $title );
-    my $template = $self->_get_template_name();
 
+sub _load_feed {
+    my ( $self, $url ) = @_;
     my ( $feed );
     if ( $url =~ m|^file://| ) {
         my $file = $url;
@@ -57,23 +84,14 @@ sub process {
         $log->is_info && $log->info( "Loading feed from URL '$url'" );
         $feed = XML::Feed->parse( URI->new( $url ) )
     }
-
     unless ( $feed ) {
         my $error = XML::Feed->errstr();
         $log->error( "Failed to load feed: ",  );
-        return "Failed to load feed from '$url': $error";
+        die "Failed to load feed from '$url': $error\n";
     }
-
-    $log->is_info && $log->info( "Loaded feed ok" );
-    $title = $feed->title || $self->_get_title();
-    my %params = (
-        feed  => $feed,
-        title => $title,
-    );
-    $log->is_debug &&
-        $log->debug( "Generating feed content using template '$template'" );
-    return $self->generate_content( \%params, { name => $template } );
+    return $feed;
 }
+
 
 sub _get_template_name {
     my ( $self ) = @_;
@@ -104,6 +122,7 @@ sub _get_template_name {
     return $template;
 }
 
+
 sub _get_default_template_content {
     return <<'CONTENT';
 [%- rss_display_cap = OI.action_param( 'num_display' ) || feed.entries.size;
@@ -119,10 +138,8 @@ sub _get_default_template_content {
 CONTENT
 }
 
-# If we return an empty title that means use the title from the feed
-
 sub _get_title {
-    my ( $self ) = @_;
+    my ( $self, $feed ) = @_;
     my $title = $self->param( 'title' );
     unless ( $title ) {
         my $title_key = $self->param( 'title_key' );
@@ -132,6 +149,9 @@ sub _get_title {
     }
     return $title;
 }
+
+# For overriding...
+sub _modify_template_params {}
 
 1;
 
@@ -191,12 +211,66 @@ feed for us and parse it into an object. That object is passed to your
 template as 'feed' and you can then iterate over the items in the feed
 (with the C<entries()> method). See the L<XML::Feed> docs for more.
 
+Note that the 'cache_expire' property (STRONGLY recommended) applies
+to the generated content.
+
+=head2 Usage method: fetch feed separately
+
+It doesn't seem that we can give L<XML::Feed> a timeout value, so you
+may find it a good idea to decouple fetching the feed from parsing the
+feed:
+
+=over 4
+
+=item *
+
+Assume we start with the following action (which specifies a box):
+
+ [delicious_links]
+ action_type  = rss
+ feed_url     = http://del.icio.us/rss/cwinters/
+ title        = Recent Links
+ cache_expire = 180m
+ template     = my_custom::delicious
+ url_none     = yes
+ weight       = 2
+ num_display  = 8
+
+=item *
+
+Using C<cron> to fetch the RSS/Atom feed using C<wget>; the following
+quietly retrieves the feed every 30 minutes:
+
+ $ crontab -l
+ 
+ # DO NOT EDIT THIS FILE - edit the master and reinstall.
+ # (/tmp/crontab.XXXXQmyjPY installed on Wed Dec  8 08:46:34 2004)
+ # (Cron version V5.0 -- $Id: crontab.c,v 1.12 2004/01/23 18:56:42 vixie Exp $)
+ */30 * * * * /usr/bin/wget -q -O /tmp/delicious-cwinters.xml http://del.icio.us/rss/cwinters
+
+Adjust the time to whatever's appropriate for your feed source.
+
+=item *
+
+Modify your action to reference the file:
+
+ [delicious_links]
+ action_type = rss
+ feed_url    = file:///tmp/delicious-cwinters.xml
+ ...
+
+=item *
+
+Restart the server; you're good to go.
+
+=back
+
 =head1 OBJECT METHODS
 
 B<process()>
 
-Retrieve the feed from the given location or cache, parse it and send
-it to a template. The parameters you can use to control this are:
+Retrieve the feed from the given location, parse it and send it to a
+template. The parameters you can use to control this are:
 
 =over 4
 
@@ -244,13 +318,57 @@ Here is the default TT template we use:
 To use your own specify a 'template' parameter in the action. This
 template name should be in the normal 'package::template' format.
 
+=head1 SUBCLASSING
+
+If you want to modify how this action does some of its job you can
+subclass it and override some parts of C<process()>. Here are the
+hooks available to you:
+
+B<_get_feed_url()>
+
+Should return the URL for this feed. By default we use the value of
+the action parameter 'feed_url'.
+
+If you cannot determine a feed URL you should C<die> with an
+appropriate message.
+
+B<_get_template_name()>
+
+Should return the fully-qualified name of a template. The default
+implementation looks in the 'template' parameter and if that's empty
+will use a default template.
+
+If you cannot return a template name you should C<die> with an
+appropriate message.
+
+B<_get_title()>
+
+Return the feed title; if none returned we use the title from the
+L<XML::Feed> object.
+
+B<_modify_template_params( \%params )>
+
+Gives you a chance to modify the parameters passed to the
+template. The hashref C<\%params> will contain the keys 'feed' (with
+an L<XML::Feed> object) and 'title' (with the feed title). You can add
+new parameters, modify the feed or title, whatever you want.
+
+B<_load_feed( $url )>
+
+Should return an L<XML::Feed> object. Note that the implementation in
+this class takes care of 'file://' URL references as well as caching,
+so you might not want to override.
+
+If you cannot return an L<XML::Feed> object you should C<die> with an
+appropriate message.
+
 =head1 SEE ALSO
 
 L<XML::Feed>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2004 Chris Winters. All rights reserved.
+Copyright (c) 2004-5 Chris Winters. All rights reserved.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
